7b30453dd341772442b57fb5bce4af8dce94e496
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-04-04 12:49:10 -0300

    Installed babel, babel node, nodemon - create a scrip entry for develop (npm start) -create an entry for prod (npm run build)

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
+//# sourceMappingURL=index.js.map
