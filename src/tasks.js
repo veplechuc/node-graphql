c6f485bb70647e5a8024e66ce38fabe87f7cc946
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-04-08 12:04:40 -0300

    file containing sample info

---
+++ src/tasks.js
@@ -0,0 +1,18 @@
+export const tasks = [
+    { id : 0,
+        name: 'tarea',
+        description: 'data description',
+        number: 1
+    },
+    { id : 1,
+        name: 'una cosa',
+        description: 'data more description',
+        number: 10
+    },
+    { id : 2,
+        name: 'otra cosa',
+        description: 'data de otra description',
+        number: 2525255
+    }
+];
+
